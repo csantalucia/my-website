a25cc552b71afd1cd74667d3d8260b07172d6e3b
csantalucia <99753511+csantalucia@users.noreply.github.com>
2025-07-06 01:15:09 -0400

    major updates

--- nav.js
+++ nav.js
@@ -6,3 +6,12 @@ fetch('navigation.html')
   .catch(error => {
     console.error('Error loading header:', error);
   });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const currentPage = location.pathname.split("/").pop(); // e.g. "about.html"
+  document.querySelectorAll("nav a").forEach(link => {
+    if (link.getAttribute("href") === currentPage) {
+      link.classList.add("active");
+    }
+  });
+});
